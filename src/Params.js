b7ae258f0bd7bd706f9e42eacb5d32a230d59560
Martin <martinczabadaj@coursier.fr>
2023-01-02 17:40:02 +0100

    fixed bug when changin min or max and getting any values

--- src/Params.js
+++ src/Params.js
@@ -6,19 +6,19 @@ export default function Params({ params, setParams, startGame }) {
         <input
           type="text"
           value={params.min}
-          onChange={({ target }) => setParams({ ...params, min: target.value })}
+          onChange={({ target }) => setParams({ ...params, min: Number(target.value) })}
         />
         <label htmlFor="max">max</label>
         <input
           type="text"
           value={params.max}
-          onChange={({ target }) => setParams({ ...params, max: target.value })}
+          onChange={({ target }) => setParams({ ...params, max: Number(target.value) })}
         />
         <label htmlFor="timer">timer</label>
         <input
           type="text"
           value={params.timer}
-          onChange={({ target }) => setParams({ ...params, timer: target.value })}
+          onChange={({ target }) => setParams({ ...params, timer: Number(target.value) })}
         />
         <button onClick={startGame}>Commencer</button>
       </header>
